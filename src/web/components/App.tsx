66d170f195d218574829e64be72d109a921a6d31
guibos <22233599+guibos@users.noreply.github.com>
2019-09-12 13:01:46 +0200

    Add header

---
+++ src/web/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'web/styles/App.css';
+import Header from 'web/components/Header'
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header/>      
+    </div>
+  );
+}
+    
+export default App;
